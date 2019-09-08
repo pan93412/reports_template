6e4b16de0ccff602ae94971011c9b8dd0dbd2f16
pan93412 <pan93412@gmail.com>
2019-09-09 00:19:05 +0800

    feat(webpack,package.json): improve package.json and add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	// production 預定
 	mode: 'development',
 
 	entry: {
@@ -39,9 +16,10 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist')
 	},
 
-	// ProgressPlugin 賊好用
+	// ProgressPlugin 👍
 	plugins: [
 		new webpack.ProgressPlugin(),
+		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			template: 'src/views/index.pug'
 		})
