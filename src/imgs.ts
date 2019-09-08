07a7049b6af0f9eb4aab7bd0ed142607b7d6079e
pan93412 <pan93412@gmail.com>
2019-09-08 14:15:47 +0800

    feat: add images

---
+++ src/imgs.ts
@@ -0,0 +1,39 @@
+/**
+  imgs.ts - readme
+
+  {
+    '[class-name]': {
+      setAs: 'bg|imgsrc',
+      imagePath: '[image-url]'
+    }
+  }
+
+  [class-name]:
+    the class of the element to insert the image.
+
+  setAs:
+    where to be inserted?
+    - bg: insert to the `background-image` of `style` property.
+    - imgsrc: insert to the `src` property of `img` element.
+  
+  imagePath:
+    where is the image?
+
+    this template has been set up file-loader, so you
+    can just import a image file the below way:
+
+        import imgPath from 'path'
+*/
+
+// YOU MUST NOT REMOVE THIS LINE!
+/// <reference path="imgs.d.ts"/>
+
+// The images to insert into reports page.
+import headerLevelLeftImg from './assets/headerImg1.png'
+
+export default {
+  'header-level-left-img': {
+    setAs: 'bg',
+    imagePath: headerLevelLeftImg
+  }
+}
--- src/js.ts
+++ src/js.ts
@@ -1,1 +1,15 @@
-// Currently nothing here.
+// Load images database.
+import img from './imgs'
+
+interface imageObject {
+  [key: string]: string
+}
+
+function applyImgsToPage <T extends imageObject> (imgObject: T): void {
+  for (const img in imgObject) {
+    const elem: HTMLElement = document.querySelector('.' + img)
+    elem.setAttribute('src', imgObject[img])
+  }
+}
+
+applyImgsToPage(img)
